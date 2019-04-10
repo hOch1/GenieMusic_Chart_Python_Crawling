1cd9f2009b68e176a00c42f36edd19528cf0a3ec
root <root@localhost.localdomain>
2019-04-11 00:13:04 +0900

    지니뮤직 차트 크롤링

---
+++ GenieCrowling.py
@@ -0,0 +1,13 @@
+import requests 
+from bs4 import BeautifulSoup 
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'} 
+url = 'https://www.genie.co.kr/chart/top200' 
+resp = requests.get(url, headers = headers) 
+soup = BeautifulSoup(resp.text, 'html.parser') 
+
+songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr') 
+
+for song in songs: 
+	title = song.find('td',{'class':'info'}).find('a',{'class':'title ellipsis'}).text 		print(title.strip())
+
